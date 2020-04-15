6aa9fce35856c43b67c20006ca83e52f21851c48
jmht <linucks42@gmail.com>
2020-04-15 12:01:53 +0100

    Further updates and simplification

--- InfluxDB/src/main.py
+++ InfluxDB/src/main.py
@@ -20,9 +20,6 @@ from machine import Pin
 from onewire import OneWire
 from onewire import DS18X20  # Liquid temperature.
 
-import config as CONFIG
-
-
 # def reset_wlan(wlan):
 #     """Reset the WLAN to the default settings"""
 #     # wlan.deinit()
@@ -58,8 +55,12 @@ LED = {'amber' : 0xFF8000,
        'blue'  : 0x0000FF,
        'green' : 0x00FF00,
        'red'   : 0xFF0000 }
-#chrono = Timer.Chrono()
 
+NETWORK_SSID = "virginmedia7305656"  # Router broadcast name.
+NETWORK_KEY = "vbvnqjxn"  # Access key.
+NTP_ADDRESS = "pool.ntp.org"
+SENSOR_INTERVAL = 60 * 10  # in seconds
+STATION_MAC = binascii.hexlify(machine.unique_id()).decode("utf-8")
 
 rtc = machine.RTC()  # Get date and time from server.
 board = Pysense()
@@ -67,36 +68,31 @@ light_sensor = LTR329ALS01(board)
 barometer = MPL3115A2(board, mode=PRESSURE)
 humidity_sensor = SI7006A20(board)
 
-# Initialisation.
-if CONFIG.STATION_MAC is None:
-    CONFIG.STATION_MAC = ubinascii.hexlify(machine.unique_id())
-
 wlan = WLAN(mode=WLAN.STA)
 nets = wlan.scan()
 for net in nets:
-    if net.ssid == CONFIG.NETWORK_SSID:
+    if net.ssid == NETWORK_SSID:
         print('Network found!')
-        wlan.connect(net.ssid, auth=(net.sec, CONFIG.NETWORK_KEY), timeout=5000)
+        wlan.connect(net.ssid, auth=(net.sec, NETWORK_KEY), timeout=5000)
         while not wlan.isconnected():
             machine.idle() # save power while waiting
         print(NETWORK_CONFIG_STR.format(*wlan.ifconfig()))
 
-rtc.ntp_sync(NTP_SERVER)
+rtc.ntp_sync(NTP_ADDRESS)
 ntp_synced = False
 if rtc.synced():
     ntp_synced = True
 
 include_timestamp = ntp_synced
-sensor_interval = 10
 while True:
     d = take_readings()
     data = ""
     for k, v in d.items():
         # k = CONFIG.SENSORS[k]
         data += 'fu_sensor,stationid={},sensor={} value={}' \
-               .format(CONFIG.STATION_MAC,k,v)
+               .format(STATION_MAC,k,v)
         if include_timestamp is True:
-            data += ' {}000000000'.format(self.timestamp)
+            data += ' {}000000000'.format(rtc.now())
         data += "\n"
     print('sending data\n{}'.format(data))
     influx_url = 'http://192.168.0.7:8086/write?db=fudata'
@@ -110,4 +106,4 @@ while True:
             print('network error: {}'.format(e.errno))
             number_of_retries -= 1
             pass
-    time.sleep(sensor_interval)
+    time.sleep(SENSOR_INTERVAL)
