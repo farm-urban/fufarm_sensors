86199ada48c788fb406778f7682e81c364375eae
jmht <linucks42@gmail.com>
2020-05-05 17:33:48 +0100

    Fix for external sensors

--- InfluxDB/pysense/main.py
+++ InfluxDB/pysense/main.py
@@ -4,7 +4,6 @@ import machine
 import utime
 from machine import Pin
 from network import WLAN
-import sys
 
 import hcsr04
 import urequests
@@ -13,12 +12,13 @@ from LTR329ALS01 import LTR329ALS01  # Ambient Light
 from MPL3115A2 import MPL3115A2, PRESSURE  # Barometer and temperature
 from SI7006A20 import SI7006A20  # Humidity & temperature.
 
-
-LED = { 'amber' : 0xFF8000,
-        'black' : 0x000000,
-        'blue'  : 0x0000FF,
-        'green' : 0x00FF00,
-        'red'   : 0xFF0000 }
+LED = {
+    'amber': 0xFF8000,
+    'black': 0x000000,
+    'blue': 0x0000FF,
+    'green': 0x00FF00,
+    'red': 0xFF0000
+}
 
 NETWORK_CONFIG_STR = """Network config:
 IP          : {0}
@@ -29,17 +29,20 @@ DNS server  : {3}"""
 
 def reset_wlan(wlan=None):
     """Reset the WLAN to the default settings"""
-    args = { 'mode' : WLAN.AP,
-             'ssid' : "wipy-wlan-1734",
-             'auth' : (WLAN.WPA2, "www.pycom.io"),
-             'channel' : 6,
-             'antenna' : WLAN.INT_ANT }
+    args = {
+        'mode': WLAN.AP,
+        'ssid': "wipy-wlan-1767 ",
+        'auth': (WLAN.WPA2, "www.pycom.io"),
+        'channel': 6,
+        'antenna': WLAN.INT_ANT
+    }
     if wlan:
         # wlan.deinit()
         wlan.init(**args)
     else:
         WLAN(**args)
     return
+
 
 def connect_wireless(wlan):
     nets = wlan.scan()
@@ -48,7 +51,7 @@ def connect_wireless(wlan):
             print('Found network: {}'.format(NETWORK_SSID))
             wlan.connect(net.ssid, auth=(net.sec, NETWORK_KEY), timeout=5000)
             while not wlan.isconnected():
-                machine.idle() # save power while waiting
+                machine.idle()  # save power while waiting
             print(NETWORK_CONFIG_STR.format(*wlan.ifconfig()))
     return wlan.isconnected()
 
@@ -57,13 +60,15 @@ def internal_sensor_readings():
     # pycom.rgbled(LED['green'])
     # print("Take readings")
     l0, l1 = light_sensor.light()
-    return { 'barometer_pressure' : barometer.pressure(),
-             'barometer_temperature' : barometer.temperature(),
-             'humidity_humidity' : humidity_sensor.humidity(),
-             'humidity_temperature' : humidity_sensor.temperature(),
-             'ambient_light_0' : l0,
-             'ambient_light_1' : l1 }
-    # pycom.rgbled(LED['black'])
+    return {
+        'barometer_pressure': barometer.pressure(),
+        'barometer_temperature': barometer.temperature(),
+        'humidity_humidity': humidity_sensor.humidity(),
+        'humidity_temperature': humidity_sensor.temperature(),
+        'ambient_light_0': l0,
+        'ambient_light_1': l1
+    }
+
 
 def send_data(iline):
     print('sending data\n{}'.format(iline))
@@ -80,11 +85,12 @@ def send_data(iline):
             pass
     return success
 
+
 def readings_to_influxdb_line(readings, station_id, include_timestamp=False):
     data = ""
     for k, v in readings.items():
         data += 'fu_sensor,stationid={},sensor={} measurement={}' \
-               .format(station_id,k,v)
+               .format(station_id, k, v)
         if include_timestamp is True:
             timestamp = utime.mktime(rtc.now())
             data += ' {}000000000'.format(timestamp)
@@ -100,7 +106,7 @@ def flow_rate(sample_window):
 
 def rate_pin_cb(arg):
     """Increment rate_cnt"""
-    #print("got an interrupt in pin %s value %s" % (arg.id(), arg()))
+    # print("got an interrupt in pin %s value %s" % (arg.id(), arg()))
     global rate_cnt, rate_pin_id
     if arg.id() == rate_pin_id:
         rate_cnt += 1
@@ -137,15 +143,17 @@ if wlan.isconnected():
     if rtc.synced():
         ntp_synced = True
 
-# initialise Ultrasonic Sensor pins
-us_trigger_pin = Pin('P4', mode=Pin.OUT)
-us_echo_pin = Pin('P10', mode=Pin.IN, pull=Pin.PULL_DOWN)
-# Initialise flow sensors
-rate_pin = Pin('P11', mode=Pin.IN, pull=Pin.PULL_UP) # Lopy4 specific: Pin('P20', mode=Pin.IN)
-rate_pin_id = rate_pin.id()
-# Pin seems to occasionally get 'stuck' on low so we just measure a
-# transition and log that, as it doens't matter if it's going 1->0 or 0->1
-rate_pin.callback(Pin.IRQ_FALLING, rate_pin_cb)
+if HAVE_EXTERNAL_SENSORS:
+    # initialise Ultrasonic Sensor pins
+    us_trigger_pin = Pin('P4', mode=Pin.OUT)
+    us_echo_pin = Pin('P10', mode=Pin.IN, pull=Pin.PULL_DOWN)
+    # Initialise flow sensors
+    rate_pin = Pin('P11', mode=Pin.IN,
+                   pull=Pin.PULL_UP)  # Lopy4 specific: Pin('P20', mode=Pin.IN)
+    rate_pin_id = rate_pin.id()
+    # Pin seems to occasionally get 'stuck' on low so we just measure a
+    # transition and log that, as it doens't matter if it's going 1->0 or 0->1
+    rate_pin.callback(Pin.IRQ_FALLING, rate_pin_cb)
 
 rate_cnt = 0
 sample_end = 0
@@ -156,7 +164,7 @@ loop_count = 0
 while True:
     pycom.heartbeat(True)
     loop_count += 1
-    print("Running loop ",loop_count)
+    print("Running loop ", loop_count)
     if not wlan.isconnected():
         print("Lost network connection")
         connect_wireless(wlan)
@@ -173,14 +181,19 @@ while True:
     while utime.time() < sample_end:
         # The flow rate is calculated using the callback within this loop
         if utime.time() - loop_time >= distance_sample_interval:
-            distance_samples.append(hcsr04.distance_measure(us_trigger_pin, us_echo_pin))
+            if HAVE_EXTERNAL_SENSORS:
+                distance_samples.append(
+                    hcsr04.distance_measure(us_trigger_pin, us_echo_pin))
             loop_time = utime.time()
     pycom.heartbeat(False)
     pycom.rgbled(LED['blue'])
     readings = internal_sensor_readings()
-    readings['distance'] = hcsr04.distance_median(distance_samples)
-    readings['flow_rate'] = flow_rate(SAMPLE_WINDOW)
-    iline = readings_to_influxdb_line(readings, STATION_MAC, include_timestamp=include_timestamp)
+    if HAVE_EXTERNAL_SENSORS:
+        readings['distance'] = hcsr04.distance_median(distance_samples)
+        readings['flow_rate'] = flow_rate(SAMPLE_WINDOW)
+    iline = readings_to_influxdb_line(readings,
+                                      STATION_MAC,
+                                      include_timestamp=include_timestamp)
     success = send_data(iline)
     if success:
         pycom.rgbled(LED['green'])
