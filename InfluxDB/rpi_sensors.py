b4b3c5d7119673eead18e4b0eeb0ce9e20d3d8fc
jmht <linucks42@gmail.com>
2020-05-07 17:09:43 +0100

    Add in sleep to get distance sensor to work

--- InfluxDB/rpi_sensors.py
+++ InfluxDB/rpi_sensors.py
@@ -9,8 +9,7 @@ https://gpiozero.readthedocs.io/en/stable/api_input.html#distancesensor-hc-sr04
 https://gpiozero.readthedocs.io/en/stable/api_pins.html#gpiozero.pins.pigpio.PiGPIOFactory
 sudo apt-get install  python3-gpiozero python3-pigpio
 # Need to start as a service
-sudo systemctl enable pigpiod
-sudo systemctl start pigpiod
+sudo pigpiod
 
 # waterproof JSN-SR04T
 https://www.amazon.co.uk/Youmile-Measuring-Transducer-Ultrasonic-Waterproof/dp/B07YDG53MC/
@@ -20,19 +19,19 @@ from gpiozero import DistanceSensor
 from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import DigitalInputDevice
 import requests
-import datetime
 import time
 
 
 INFLUX_URL = 'http://10.8.0.1:8086/write?db=farmdb'
 STATION_MAC = 'rpizero1utc'
 SAMPLE_WINDOW = 60 * 5
+MOCK = False
 rate_cnt = 0
-
 
 def send_data(iline):
     print('sending data\n{}'.format(iline))
-    # return
+    if MOCK:
+       return
     success = False
     number_of_retries = 3
     while not success and number_of_retries > 0:
@@ -45,14 +44,13 @@ def send_data(iline):
             pass
     return success
 
-
 def readings_to_influxdb_line(readings, station_id, include_timestamp=False):
     data = ""
     for k, v in readings.items():
         data += 'fu_sensor,stationid={},sensor={} measurement={}' \
-               .format(station_id, k, v)
+               .format(station_id,k,v)
         if include_timestamp is True:
-            timestamp = datetime.datetime.utcnow()
+            timestamp = utime.mktime(rtc.now())
             data += ' {}000000000'.format(timestamp)
         data += "\n"
     return data
@@ -61,11 +59,10 @@ def readings_to_influxdb_line(readings, station_id, include_timestamp=False):
 def count_paddle():
     global rate_cnt
     rate_cnt += 1
-
+    #print("button was pressed")
 
 def flow_rate(sample_window):
     return rate_cnt
-
 
 btn = DigitalInputDevice(22)
 btn.when_activated = count_paddle
@@ -80,7 +77,8 @@ while True:
     rate_cnt = 0
     while time.time() < sample_end:
         pass
-    readings['distance'] = sensor.distance
     readings['flow_rate'] = flow_rate(SAMPLE_WINDOW)
+    time.sleep(1) # Need to add in pause or the distance sensor or else it measures 0.0
+    readings['distance'] = sensor.distance
     iline = readings_to_influxdb_line(readings, STATION_MAC)
     success = send_data(iline)
