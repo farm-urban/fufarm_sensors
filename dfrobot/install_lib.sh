1fd9febf5001ebd33a76ad4a5f333c96c41ddbb5
linucks <linucks42@gmail.com>
2021-03-05 16:05:51 +0000

    Updated to work with own fixes to dfrobot code

--- dfrobot/install_lib.sh
+++ dfrobot/install_lib.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 # arduino-cli lib search debouncer
+LIBDIR=$HOME/Arduino/libraries
+
 # For SEN0137 Temperature and Humidity sensor
 arduino-cli lib install "DHT sensor library for ESPx"
+
 # For DFR0198: Waterproof DS18B20 Sensor Kit
 arduino-cli lib install OneWire
-# Json
+
+# JSON
 arduino-cli lib install ArduinoJson
 
 # DEFOBOT EC
-wget https://github.com/DFRobot/DFRobot_EC/archive/master.zip
-unzip master.zip
-mv DFRobot_EC-master/ ~/Arduino/libraries/
+#wget https://github.com/DFRobot/DFRobot_EC/archive/master.zip
+#unzip master.zip
+#rm master.zip
+#mv DFRobot_EC-master $LIBDIR
+git clone https://github.com/linucks/DFRobot_EC.git $LIBDIR/DFRobot_EC
+
+# DEFOBOT pH
+git clone https://github.com/linucks/DFRobot_PH.git $LIBDIR/DFRobot_PH
