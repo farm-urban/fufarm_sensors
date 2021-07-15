864f9d2a30cdc3df1357b062cfc17c18e62d9a6f
jmht <linucks42@gmail.com>
2021-07-15 13:01:17 +0100

    Cleaned up and updated to reconnect on wifi disconnect

--- dfrobot/esp8266pwr/esp8266pwr.ino
+++ dfrobot/esp8266pwr/esp8266pwr.ino
@@ -3,15 +3,88 @@
 #include <SPI.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <Adafruit_ADS1X15.h>
+#include <Wire.h>
 
 // Time in milliseconds - 5 minutes = 1000 * 60 * 5 = 300000
 #define SAMPLE_WINDOW 5000 // 5 seconds for testing
-char ssid[] = "**********";           // SSID of your home WiFi
-char password[] = "*********";            // password of your home WiFi
+#define SSID "Vertical Farm Pilot_EXT"
+#define WIFI_PASSWORD "1106FaRm1028"
+#define MQTT_CLIENT_NAME "h2Pwr"
+#define MQTT_USER "mosquitto"
+#define MQTT_PASSWORD "mosquitto"
+
+// Define Variables
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+IPAddress mqttServer(192, 168, 1, 225);
+Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */  
+int16_t adc0;
+float Current = 0;
+
+// Define Functions
+void ADC()
+{
+    adc0 = ads.readADC_SingleEnded(0);
+    int average = 0;
+    for (int i=0; i < 1000; i++) {
+      average = average + adc0;
+    }
+    average = average/1000;
+    Current = (average - 1659) * 0.0866;
+} // END ADC
 
 
-// Update these with values suitable for your network.
-IPAddress mqttServer(172, 16, 0, 2);
+void connectToWiFi()
+{
+  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
+    would try to act as both a client and an access-point and could cause
+    network-issues with your other WiFi-devices on your WiFi-network. */
+  WiFi.mode(WIFI_STA);
+
+  int i = 0;
+  while ( WiFi.status() != WL_CONNECTED )
+  {
+    WiFi.disconnect(); // https://forum.arduino.cc/t/mqtt-with-esp32-gives-timeout-exceeded-disconnecting/688723/6
+    Serial.print("Connecting to ");
+    Serial.println(SSID);
+    WiFi.begin(SSID, WIFI_PASSWORD);
+    delay(4000);
+    i++;
+    if ( i == 3 )
+    {
+      Serial.print("*** Restarting ESP on failed WIFI connect ***");
+      ESP.restart();
+    }
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+} // END connectToWiFi
+
+
+void reconnectMQTT() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(MQTT_CLIENT_NAME, MQTT_USER, MQTT_PASSWORD)) {
+      Serial.println("MQTT connected");
+      ADC(); //get sensor data
+      if (Current < 0) Current = 0;
+      char charCurrent[] = "0.00" ; // convert Current float to string
+      dtostrf(Current, 6, 1, charCurrent);
+      // Once connected, publish an announcement...
+      client.publish("H2Pwr", charCurrent);
+    } else {
+        Serial.print("failed, rc=");
+        Serial.print(client.state());
+        Serial.println(" try again in 5 seconds");
+        // Wait 5 seconds before retrying
+        delay(5000);
+    }
+  }
+} // END reconnectMQTT
 
 //void callback(char* topic, byte* payload, unsigned int length) {
 //  Serial.print("Message arrived [");
@@ -23,63 +96,36 @@ IPAddress mqttServer(172, 16, 0, 2);
 //  Serial.println();
 //}
 
-WiFiClient wifiClient;
-PubSubClient client(wifiClient);
-
-void reconnect() {
-  
-  // Loop until we're reconnected
-  while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Attempt to connect
-    if (client.connect("h2Pwr", "mosquitto", "mosquitto")) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("h2pwr/status","hello world");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
-    }
-  }
-}
-
 void setup()
 {
-  Serial.begin(9600);
-
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
-     would try to act as both a client and an access-point and could cause
-     network-issues with your other WiFi-devices on your WiFi-network. */
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
-
+  Serial.begin(57600);
+  connectToWiFi();
   client.setServer(mqttServer, 1883);
 //  client.setCallback(callback);
 
   // Allow the hardware to sort itself out
   delay(1500);
-}
+
+  Serial.println("Getting single-ended readings from AIN0..3");
+  // ads1015.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV (default)
+  // ads1015.setGain(GAIN_ONE);     // 1x gain   +/- 4.096V  1 bit = 2mV
+  // ads1015.setGain(GAIN_TWO);     // 2x gain   +/- 2.048V  1 bit = 1mV
+  // ads1015.setGain(GAIN_FOUR);    // 4x gain   +/- 1.024V  1 bit = 0.5mV
+  // ads1015.setGain(GAIN_EIGHT);   // 8x gain   +/- 0.512V  1 bit = 0.25mV
+  // ads1015.setGain(GAIN_SIXTEEN); // 16x gain  +/- 0.256V  1 bit = 0.125mV
+  ads.setGain(GAIN_TWO);
+  ads.begin();
+  float Current =0;
+} // END setup
 
 void loop()
 {
+  if ( WiFi.status() != WL_CONNECTED ) {
+    connectToWiFi();
+  }
   if (!client.connected()) {
-    reconnect();
+    reconnectMQTT();
   }
   client.loop();
   delay(SAMPLE_WINDOW);
-}
+} // END loop
