534007dc18b75112a5ce8045d05e1a3955c6d306
jmht <linucks42@gmail.com>
2021-08-05 12:44:59 +0100

    Updated to parse different log formats

--- dfrobot/raspberrypi/bluelab_logs.py
+++ dfrobot/raspberrypi/bluelab_logs.py
@@ -3,6 +3,7 @@
 from collections import namedtuple
 import csv
 import datetime
+from enum import Enum
 import os
 from pathlib import Path
 import sys
@@ -11,6 +12,12 @@ import warnings
 
 LOGDIR = '/home/pi/.local/share/Bluelab/Connect/logs'
 GrowData = namedtuple('GrowData', ['tag', 'timestamp', 'ec', 'ph', 'temp'])
+
+class ParseMode(Enum):
+    MONITOR1 = 1
+    MONITOR2 = 2
+    CONTROL1 = 3
+    CONTROL2 = 4
 
 def get_last_log(logdir: Union[str, Path]):
 
@@ -27,82 +34,177 @@ def get_last_log(logdir: Union[str, Path]):
 
 
 def get_data(csv_file: Union[str, Path]):
-    """
-With one system:
-Time (Europe/London),Conductivity unit,Temperature unit,52rf Conductivity,52rf pH,52rf Temperature
-2021-07-04 15:33:00,EC,C,---,7.7,---
-
-With two systems:
-Time (Europe/London),Conductivity unit,Temperature unit,52rf Conductivity,52rf pH,52rf Temperature, run time, run time, run time,4q3f Conductivity,4q3f pH,4q3f Temperature, run time, run time, run time
-2021-07-11 18:23:00,EC,C,1.3,8.2,21,0.0,0.0,0.0,1.3,8.1,21,0.0,0.0,0.0
+    """See bluelab_logs directory for csv files with format
     """
     if not isinstance(csv_file, Path):
         csv_file = Path(csv_file)
-    data = []
-    systems = []
+    readings = []
     with open(csv_file) as f:
         reader = csv.reader(f)
+        mode, systems = parse_header(reader)
         for i, row in enumerate(reader):
-            if i == 0:
-                ncol = len(row)
-                # ncol 6 is monitor 1
-                # ncol 9 is monitor 2 and control 1
-                # ncol 15 is control 2
-                print("NROWS IS ",len(row))
-                tag1 = row[3].split()[0]
-                systems.append(tag1)
-                if len(row) == 6:
-                    pass # 1 system
-                elif len(row) == 15:
-                    tag2 = row[9].split()[0]
-                    systems.append(tag2)
-                else:
-                    raise RuntimeError("Unrecognised csv format - too many systems?")
-                continue
-            if i == 1:
-                assert row[1] == 'EC', f"Incorrect EC unit: {row[1]}"
-                assert row[2] == 'C', f"Incorrect temp unit: {row[2]}"
-            timestamp = datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S') 
-            try:
-                ec1 = float(row[3])
-            except ValueError:
-                warnings.warn(f"Invalid EC value: {row[3]}")
-                ec1 = -9999
-            try:
-                ph1 = float(row[4])
-            except ValueError:
-                warnings.warn(f"Invalid pH value: {row[4]}")
-                ph1 = -9999
-            try:
-                temp1 = float(row[5])
-            except ValueError:
-                warnings.warn(f"Invalid temp value: {row[5]}")
-                temp1 = -9999
+            assert row[1] == 'EC', f"Incorrect EC unit: {row[1]}"
+            assert row[2] == 'C', f"Incorrect temp unit: {row[2]}"
+            if mode == ParseMode.MONITOR1:
+                data = parse_monitor1(row, systems)
+            elif mode == ParseMode.CONTROL1:
+                data = parse_control1(row, systems)
+            elif mode == ParseMode.MONITOR2:
+                data = parse_monitor2(row, systems)
+            elif mode == ParseMode.CONTROL2:
+                data = parse_control2(row, systems)
+            readings.append(data)
+    return readings
 
-            if len(systems) == 2:
-                try:
-                    ec2 = float(row[9])
-                except ValueError:
-                    warnings.warn(f"Invalid EC2 value: {row[9]}")
-                    ec2 = -9999
-                try:
-                    ph2 = float(row[10])
-                except ValueError:
-                    warnings.warn(f"Invalid pH2 value: {row[10]}")
-                    ph2 = -9999
-                try:
-                    temp2 = float(row[11])
-                except ValueError:
-                    warnings.warn(f"Invalid temp2 value: {row[11]}")
-                    temp2 = -9999
+def parse_header(reader):
+    # Check header to determine node and get system names
+    systems = []
+    row0 = next(reader)
+    ncol = len(row0)
+    if ncol == 6:
+        mode = ParseMode.MONITOR1
+    elif ncol == 9:
+        field = row0[6].strip()
+        if field == 'run time':
+            mode = ParseMode.CONTROL1
+        else:
+            mode = ParseMode.MONITOR2
+    elif ncol == 15:
+        mode = ParseMode.CONTROL2
+    else:
+        raise RuntimeError("Unrecognised csv format - too many systems?")
 
-            d1 = GrowData(tag1, timestamp, ec1, ph1, temp1)
-            if len(systems) == 1:
-                data.append([d1])
-            elif len(systems) == 2:
-                d2 = GrowData(tag2, timestamp, ec2, ph2, temp2)
-                data.append([d1, d2])
-    return data
+    # Add system names
+    tag1 = row0[3].split()[0]
+    systems.append(tag1)
+    if mode == ParseMode.MONITOR2:
+        tag2 = row0[6].split()[0]
+        systems.append(tag2)
+    elif mode == ParseMode.CONTROL2:
+        tag2 = row0[9].split()[0]
+        systems.append(tag2)
+
+    return mode, systems
+
+def parse_monitor1(row, systems):
+    timestamp = datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S') 
+    try:
+        ec1 = float(row[3])
+    except ValueError:
+        warnings.warn(f"Invalid EC value: {row[3]}")
+        ec1 = -9999
+    try:
+        ph1 = float(row[4])
+    except ValueError:
+        warnings.warn(f"Invalid pH value: {row[4]}")
+        ph1 = -9999
+    try:
+        temp1 = float(row[5])
+    except ValueError:
+        warnings.warn(f"Invalid temp value: {row[5]}")
+        temp1 = -9999
+
+    return [GrowData(systems[0], timestamp, ec1, ph1, temp1)]
+
+
+def parse_control1(row, systems):
+    timestamp = datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S') 
+    try:
+        ec1 = float(row[3])
+    except ValueError:
+        warnings.warn(f"Invalid EC value: {row[3]}")
+        ec1 = -9999
+    try:
+        ph1 = float(row[4])
+    except ValueError:
+        warnings.warn(f"Invalid pH value: {row[4]}")
+        ph1 = -9999
+    try:
+        temp1 = float(row[5])
+    except ValueError:
+        warnings.warn(f"Invalid temp value: {row[5]}")
+        temp1 = -9999
+
+    return [GrowData(systems[0], timestamp, ec1, ph1, temp1)]
+
+
+def parse_monitor2(row, systems):
+    timestamp = datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S') 
+    try:
+        ec1 = float(row[3])
+    except ValueError:
+        warnings.warn(f"Invalid EC value: {row[3]}")
+        ec1 = -9999
+    try:
+        ph1 = float(row[4])
+    except ValueError:
+        warnings.warn(f"Invalid pH value: {row[4]}")
+        ph1 = -9999
+    try:
+        temp1 = float(row[5])
+    except ValueError:
+        warnings.warn(f"Invalid temp value: {row[5]}")
+        temp1 = -9999
+    d1 = GrowData(systems[0], timestamp, ec1, ph1, temp1)
+
+    try:
+        ec2 = float(row[6])
+    except ValueError:
+        warnings.warn(f"Invalid EC2 value: {row[6]}")
+        ec2 = -9999
+    try:
+        ph2 = float(row[7])
+    except ValueError:
+        warnings.warn(f"Invalid pH2 value: {row[7]}")
+        ph2 = -9999
+    try:
+        temp2 = float(row[8])
+    except ValueError:
+        warnings.warn(f"Invalid temp2 value: {row[8]}")
+        temp2 = -9999
+    d2 = GrowData(systems[1], timestamp, ec2, ph2, temp2)
+
+    return [d1, d2]
+
+
+def parse_control2(row, systems):
+    timestamp = datetime.datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S') 
+    try:
+        ec1 = float(row[3])
+    except ValueError:
+        warnings.warn(f"Invalid EC value: {row[3]}")
+        ec1 = -9999
+    try:
+        ph1 = float(row[4])
+    except ValueError:
+        warnings.warn(f"Invalid pH value: {row[4]}")
+        ph1 = -9999
+    try:
+        temp1 = float(row[5])
+    except ValueError:
+        warnings.warn(f"Invalid temp value: {row[5]}")
+        temp1 = -9999
+    d1 = GrowData(systems[0], timestamp, ec1, ph1, temp1)
+
+    try:
+        ec2 = float(row[9])
+    except ValueError:
+        warnings.warn(f"Invalid EC2 value: {row[9]}")
+        ec2 = -9999
+    try:
+        ph2 = float(row[10])
+    except ValueError:
+        warnings.warn(f"Invalid pH2 value: {row[10]}")
+        ph2 = -9999
+    try:
+        temp2 = float(row[11])
+    except ValueError:
+        warnings.warn(f"Invalid temp2 value: {row[11]}")
+        temp2 = -9999
+    d2 = GrowData(systems[1], timestamp, ec2, ph2, temp2)
+
+    return [d1, d2]
+
 
 def sample_bluelab_data(last_timestamp: datetime.datetime, poll_interval: int, last_log: Optional[str]=None):
     BUFFER = 30
@@ -153,26 +255,34 @@ def sample_bluelab_data(last_timestamp: datetime.datetime, poll_interval: int, l
         results.append(d2)
     return results
 
-
-if __name__ == "__main__":
-    #poll_interval = 50000 * 60
-    #last_timestamp = datetime.datetime.now() - datetime.timedelta(seconds=poll_interval)
-    #print(sample_bluelab_data(last_timestamp, poll_interval))
-
-#    poll_interval = 1
-#    target_timestamp = datetime.datetime(2021, 7, 6, 11, 38)
-#    last_timestamp = target_timestamp - datetime.timedelta(seconds=poll_interval)
-#    last_log = "bluelab_logs/monitor_one.csv"
-#    x = sample_bluelab_data(last_timestamp, poll_interval, last_log=last_log)
-#    assert x[0].timestamp == target_timestamp
-
-
+## Unit tests
+def test_control1():
     poll_interval = 1
-    target_timestamp = datetime.datetime(2021, 7, 6, 11, 38)
+    target_timestamp = datetime.datetime(2021, 7, 11, 17, 51)
     last_timestamp = target_timestamp - datetime.timedelta(seconds=poll_interval)
-    last_log = "bluelab_logs/control_two.csv"
-    last_log = "bluelab_logs/monitor_two.csv"
-    last_log = "bluelab_logs/control_one.csv"
+    last_log = "/opt/fu_sensors/dfrobot/raspberrypi/bluelab_logs/control1.csv"
     x = sample_bluelab_data(last_timestamp, poll_interval, last_log=last_log)
     assert x[0].timestamp == target_timestamp
+
+
+def test_monitor2():
+    poll_interval = 1
+    target_timestamp = datetime.datetime(2021, 8, 2, 7, 57)
+    last_log = "/opt/fu_sensors/dfrobot/raspberrypi/bluelab_logs/monitor2.csv"
+    last_timestamp = target_timestamp - datetime.timedelta(seconds=poll_interval)
+    x = sample_bluelab_data(last_timestamp, poll_interval, last_log=last_log)
+    assert x[0].timestamp == target_timestamp
+
+
+def test_control2():
+    poll_interval = 1
+    target_timestamp = datetime.datetime(2021, 8, 2, 7, 50)
+    last_log = "/opt/fu_sensors/dfrobot/raspberrypi/bluelab_logs/control2.csv"
+    last_timestamp = target_timestamp - datetime.timedelta(seconds=poll_interval)
+    x = sample_bluelab_data(last_timestamp, poll_interval, last_log=last_log)
+    assert x[0].timestamp == target_timestamp
+
+
+if __name__ == "__main__":
+    test_control2()
 
