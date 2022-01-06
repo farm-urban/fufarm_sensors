b41bbf7c464190b71eee0dc8cd23d6a64bbf7dc3
jmht <linucks42@gmail.com>
2022-01-06 09:29:13 +0000

    More refactoring

--- sensors/rpi_direct_sensors.py
+++ sensors/rpi_direct_sensors.py
@@ -1,48 +1,9 @@
-"""
-# NAT forwarding
-https://gist.github.com/kimus/9315140
-
-# Map for GPIO on pi zero
-https://cdn.sparkfun.com/assets/learn_tutorials/6/7/6/PiZerov2.pdf
-
-https://gpiozero.readthedocs.io/en/stable/api_input.html#distancesensor-hc-sr04
-https://gpiozero.readthedocs.io/en/stable/api_pins.html#gpiozero.pins.pigpio.PiGPIOFactory
-
-http://abyz.me.uk/rpi/pigpio/python.html
-sudo apt-get install  python3-gpiozero python3-pigpio
-
-# Need to start as a service
-sudo pigpiod
-
-# Wiring diagram HC-SR04
-https://thepihut.com/blogs/raspberry-pi-tutorials/hc-sr04-ultrasonic-range-sensor-on-the-raspberry-pi
-
-# YF-S201
-https://www.hobbytronics.co.uk/yf-s201-water-flow-meter
-Requires +5V so need to use voltage divider
-
-Red ---------------- 5V
-
-             +------ GPIO 22
-             |
-             |
-             |
-Yellow --1K--+--2K-- GND
-
-Black -------------- GND
-
-# waterproof JSN-SR04T
-https://www.amazon.co.uk/Youmile-Measuring-Transducer-Ultrasonic-Waterproof/dp/B07YDG53MC/
-
-"""
-from gpiozero import DistanceSensor
-from gpiozero.pins.pigpio import PiGPIOFactory
-from gpiozero import DigitalInputDevice
 import logging
 import time
 
 # local imports
 import influxdb
+import direct_sensors_rpi
 
 
 INFLUX_URL = "http://10.8.0.1:8086/write?db=farmdb"
@@ -52,8 +13,6 @@ SAMPLE_WINDOW = 60 * 5
 MOCK = False
 INFLUX_SCHEMA = {"measurement": "fu_sensors", "tags": {"station_id": STATION_MAC}}
 
-
-USE_PIGPIOD = False
 
 SAMPLE_WINDOW = 60 * 5
 SAMPLE_WINDOW = 5
@@ -84,42 +43,18 @@ logging.basicConfig(
 logger = logging.getLogger()
 
 
-def count_paddle():
-    global pulse_count
-    pulse_count += 1
-    # print("button was pressed")
-
-
-def flow_rate(sample_window):
-    """From YF-S201 manual:
-    Pluse Characteristic:F=7Q(L/MIN).
-    2L/MIN=16HZ 4L/MIN=32.5HZ 6L/MIN=49.3HZ 8L/MIN=65.5HZ 10L/MIN=82HZ
-
-    sample_window is in seconds, so hz is pulse_count / sample_window
-    """
-    hertz = pulse_count / sample_window
-    return hertz / 7.0
-
-
-btn = DigitalInputDevice(22)
-btn.when_activated = count_paddle
-
-factory = None
-if USE_PIGPIOD:
-    factory = PiGPIOFactory()
-sensor = DistanceSensor(trigger=17, echo=27, pin_factory=factory, queue_len=20)
-
-pulse_count = 0
+direct_sensors_rpi.reset_flow_counter()
 readings = {}
 while True:
     sample_start = time.time()
     sample_end = sample_start + SAMPLE_WINDOW
     pulse_count = 0
     while time.time() < sample_end:
+        # Loop to accumulate flow pulses
         pass
-    readings["flow_rate"] = flow_rate(SAMPLE_WINDOW)
+    readings["flow_rate"] = direct_sensors_rpi.flow_rate(SAMPLE_WINDOW)
     time.sleep(2)  # Need to add in pause or the distance sensor or else it measures 0.0
-    readings["distance"] = sensor.distance
+    readings["distance"] = direct_sensors_rpi.distance_sensor.distance
     influxdb.send_data_to_influx(
         influx_schema,
         MEASUREMENT,
