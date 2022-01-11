d5d81e3c41573332bd66b278b18f1b339940b51b
jmht <linucks42@gmail.com>
2022-01-11 17:17:09 +0000

    Updated for new influxdb infrastructure

--- sensors/wipy/pysense/main.py
+++ sensors/wipy/pysense/main.py
@@ -15,24 +15,44 @@ from MPL3115A2 import MPL3115A2, PRESSURE  # Barometer and temperature
 from SI7006A20 import SI7006A20  # Humidity & temperature.
 
 
-STATION_MAC = 'lfarm_2'
-NETWORK_SSID = "FUsensors"
-NETWORK_KEY = "12345678"
-INFLUX_URL = 'http://192.168.4.1:8086/write?db=farmdb'
-NTP_SERVER = 'pool.ntp.org'
+NETWORK_SSID = "LLS_BYOD"
+NETWORK_KEY = ""
+# NETWORK_SSID = "Farm Urban"
+# NETWORK_KEY = "v8fD53Rs"
+
+MOCK = False
 SAMPLE_WINDOW = 60 * 10
-HAVE_EXTERNAL_SENSORS = False
-MOUNTPOINT = '/sd'
-PATHSEP = "/"
+SENSOR_STATION_ID = "farmwipy1"
+MEASUREMENT = "sensors"
+BUCKET = "cryptfarm"
+TOKEN = "scW9V68kenPTzEkGUAtky-7awOMuo71pPGnCJ3gEdJWNNFBrlvp5atHTSFttVY4rRj0796xBgkuaF_YkSQExBg=="
+# TOKEN = open("TOKEN").readline().strip()
+ORG = "Farm%20Urban"  # Currently need to manually encode spaces
+INFLUX_URL = "http://farmuaa1.farmurban.co.uk:8086"
+influx_schema = {
+    "endpoint": INFLUX_URL,
+    "org": ORG,
+    "token": TOKEN,
+    "bucket": BUCKET,
+}
+sensor_influx_tags = {"station_id": SENSOR_STATION_ID}
+
+
 HAVE_SD = False
+HAVE_EXTERNAL_SENSORS = False
+NTP_SERVER = "pool.ntp.org"
+
+# SD variables
+MOUNTPOINT = "/sd"
+PATHSEP = "/"
 
 LED = {
-    'amber': 0xFF8000,
-    'black': 0x000000,
-    'blue': 0x0000FF,
-    'green': 0x00FF00,
-    'orange': 0xFFA500,
-    'red': 0xFF0000
+    "amber": 0xFF8000,
+    "black": 0x000000,
+    "blue": 0x0000FF,
+    "green": 0x00FF00,
+    "orange": 0xFFA500,
+    "red": 0xFF0000,
 }
 
 NETWORK_CONFIG_STR = """Network config:
@@ -45,11 +65,11 @@ DNS server  : {3}"""
 def reset_wlan(wlan=None):
     """Reset the WLAN to the default settings"""
     args = {
-        'mode': WLAN.AP,
-        'ssid': "wipy-wlan-1767 ",
-        'auth': (WLAN.WPA2, "www.pycom.io"),
-        'channel': 6,
-        'antenna': WLAN.INT_ANT
+        "mode": WLAN.AP,
+        "ssid": "wipy-wlan-1767 ",
+        "auth": (WLAN.WPA2, "www.pycom.io"),
+        "channel": 6,
+        "antenna": WLAN.INT_ANT,
     }
     if wlan:
         # wlan.deinit()
@@ -63,7 +83,7 @@ def connect_wireless(wlan):
     nets = wlan.scan()
     for net in nets:
         if net.ssid == NETWORK_SSID:
-            print('Found network: {}'.format(NETWORK_SSID))
+            print("Found network: {}".format(NETWORK_SSID))
             wlan.connect(net.ssid, auth=(net.sec, NETWORK_KEY), timeout=5000)
             while not wlan.isconnected():
                 machine.idle()  # save power while waiting
@@ -76,16 +96,18 @@ def internal_sensor_readings():
     # print("Take readings")
     l0, l1 = light_sensor.light()
     return {
-        'barometer_pressure': barometer.pressure(),
-        'barometer_temperature': barometer.temperature(),
-        'humidity_humidity': humidity_sensor.humidity(),
-        'humidity_temperature': humidity_sensor.temperature(),
-        'ambient_light_0': l0,
-        'ambient_light_1': l1
+        "barometer_pressure": barometer.pressure(),
+        "barometer_temperature": barometer.temperature(),
+        "humidity_humidity": humidity_sensor.humidity(),
+        "humidity_temperature": humidity_sensor.temperature(),
+        "ambient_light_0": l0,
+        "ambient_light_1": l1,
     }
 
 
-def take_readings(sample_window, have_external_sensors=False, distance_sample_interval=1):
+def take_readings(
+    sample_window, have_external_sensors=False, distance_sample_interval=1
+):
     global rate_count, us_trigger_pin, us_echo_pin, LED
     sample_start = utime.time()
     sample_end = sample_start + sample_window
@@ -97,44 +119,82 @@ def take_readings(sample_window, have_external_sensors=False, distance_sample_in
         if utime.time() - sample_time >= distance_sample_interval:
             if have_external_sensors:
                 distance_samples.append(
-                    hcsr04.distance_measure(us_trigger_pin, us_echo_pin))
+                    hcsr04.distance_measure(us_trigger_pin, us_echo_pin)
+                )
             sample_time = utime.time()
     readings = internal_sensor_readings()
     if have_external_sensors:
-        readings['distance'] = hcsr04.distance_median(distance_samples)
-        readings['flow_rate'] = flow_rate(sample_window)
+        readings["distance"] = hcsr04.distance_median(distance_samples)
+        readings["flow_rate"] = flow_rate(sample_window)
     return readings
 
 
-def send_data(iline):
-    print('sending data\n{}'.format(iline))
+def send_data_to_influx(
+    schema, measurement, tags, fields, timestamp=None, local_timestamp=False
+):
+    iline = readings_to_influxdb_line(
+        measurement, tags, fields, timestamp=timestamp, local_timestamp=local_timestamp
+    )
+    return send_data(schema, iline)
+
+
+def readings_to_influxdb_line(
+    measurement, tags, fields, timestamp=None, local_timestamp=False
+):
+    if timestamp and local_timestamp:
+        raise RuntimeError("Cannot include a timestamp with local_timestamp")
+
+    tags = ",".join(["{}={}".format(k, v) for k, v in tags.items()])
+    fields = ",".join(
+        ["{}={:e}".format(k, v) for k, v in fields.items() if v is not None]
+    )
+    iline = "{},{} {}".format(measurement, tags, fields)
+
+    if timestamp or local_timestamp:
+        # if timestamp and isinstance(timestamp, datetime.datetime):
+        if timestamp:
+            timestamp = int(float(timestamp.strftime("%s.%f")) * 1000000000)
+        if local_timestamp:
+            timestamp = int(utime.mktime(rtc.now()) * 100000000)
+            # timestamp = "{}000000000".format(timestamp)
+        iline += " {}".format(timestamp)
+
+    iline += "\n"
+    return iline
+
+
+def send_data(schema, iline):
+    """
+    https://docs.influxdata.com/influxdb/v2.0/api/#tag/Write
+    https://docs.influxdata.com/influxdb/v2.0/write-data/developer-tools/api/
+    """
+    url = "{}/api/v2/write".format(schema["endpoint"])
+    params = {"org": schema["org"], "bucket": schema["bucket"]}
+    headers = {"Authorization": "Token {}".format(schema["token"])}
+    url += "?" + "&".join(["{}={}".format(k, v) for k, v in params.items()])
+    print("Sending url: {} headers: {} data: {}".format(url, headers, iline))
+    if MOCK:
+        return
     success = False
+    retry = True
     number_of_retries = 3
-    while not success and number_of_retries > 0:
+    tries = 0
+    while retry:
         try:
-            # pycom.rgbled(LED['blue'])
-            urequests.post(INFLUX_URL, data=iline)
+            response = urequests.post(url, headers=headers, data=iline)
+            print("Sent data - status_code: {}".format(response.status_code))
             success = True
-        except OSError as e:
-            print('network error: {}'.format(e))
-            number_of_retries -= 1
-            pass
+            retry = False
+            break
+        except Exception as e:
+            print("Network error: {}".format(e))
+            tries += 1
+            if number_of_retries > 0:
+                retry = tries < number_of_retries
     return success
 
 
-def readings_to_influxdb_line(readings, station_id, include_timestamp=False):
-    data = ""
-    for k, v in readings.items():
-        data += 'fu_sensor,stationid={},sensor={} measurement={}' \
-               .format(station_id, k, v)
-        if include_timestamp is True:
-            timestamp = utime.mktime(rtc.now())
-            data += ' {}000000000'.format(timestamp)
-        data += "\n"
-    return data
-
-
-def next_logfile(name_stem='logfile'):
+def next_logfile(name_stem="logfile"):
     MAX_FILENAMES = 100
     count = 0
     while True:
@@ -156,8 +216,8 @@ def next_logfile(name_stem='logfile'):
 def init_logfile(readings, station_id):
     " Open the csv logfile and initialise it with the headings"
     logfile_path = next_logfile()
-    logfile = open(logfile_path, 'w')
-    headings = ['timestamp', 'stationid'] + sorted(readings.keys())
+    logfile = open(logfile_path, "w")
+    headings = ["timestamp", "stationid"] + sorted(readings.keys())
     logfile.write(",".join(headings) + "\n")
     logfile.flush()
     return logfile
@@ -165,7 +225,9 @@ def init_logfile(readings, station_id):
 
 def write_readings_to_logfile(logfile, readings, station_id):
     timestamp = str(utime.mktime(rtc.now()))
-    values = [timestamp, station_id] + [str(readings[k]) for k in sorted(readings.keys())]
+    values = [timestamp, station_id] + [
+        str(readings[k]) for k in sorted(readings.keys())
+    ]
     logfile.write(",".join(values) + "\n")
     logfile.flush()
     return
@@ -184,17 +246,18 @@ def rate_pin_cb(arg):
     if arg.id() == rate_pin_id:
         rate_count += 1
 
+
 ################################################################################
 # Start Script
 ################################################################################
-if STATION_MAC is None:
-    STATION_MAC = binascii.hexlify(machine.unique_id()).decode("utf-8")
+if SENSOR_STATION_ID is None:
+    SENSOR_STATION_ID = binascii.hexlify(machine.unique_id()).decode("utf-8")
 
 if not HAVE_EXTERNAL_SENSORS:
     hcsr04.MOCK = True
 
 pycom.heartbeat(False)
-pycom.rgbled(LED['amber'])
+pycom.rgbled(LED["amber"])
 rtc = machine.RTC()  # Get date and time from server.
 board = Pysense()
 light_sensor = LTR329ALS01(board)
@@ -211,11 +274,12 @@ if wlan.isconnected():
 
 if HAVE_EXTERNAL_SENSORS:
     # initialise Ultrasonic Sensor pins
-    us_trigger_pin = Pin('P4', mode=Pin.OUT)
-    us_echo_pin = Pin('P10', mode=Pin.IN, pull=Pin.PULL_DOWN)
+    us_trigger_pin = Pin("P4", mode=Pin.OUT)
+    us_echo_pin = Pin("P10", mode=Pin.IN, pull=Pin.PULL_DOWN)
     # Initialise flow sensors
-    rate_pin = Pin('P11', mode=Pin.IN,
-                   pull=Pin.PULL_UP)  # Lopy4 specific: Pin('P20', mode=Pin.IN)
+    rate_pin = Pin(
+        "P11", mode=Pin.IN, pull=Pin.PULL_UP
+    )  # Lopy4 specific: Pin('P20', mode=Pin.IN)
     rate_pin_id = rate_pin.id()
     # Pin seems to occasionally get 'stuck' on low so we just measure a
     # transition and log that, as it doens't matter if it's going 1->0 or 0->1
@@ -232,10 +296,10 @@ if HAVE_SD:
     uos.mount(sd, MOUNTPOINT)
     readings = take_readings(3, have_external_sensors=HAVE_EXTERNAL_SENSORS)
     print("Setting up logfile")
-    logfile = init_logfile(readings, STATION_MAC)
+    logfile = init_logfile(readings, SENSOR_STATION_ID)
 
 rate_count = 0
-include_timestamp = ntp_synced
+LOCAL_TIMESTAMP = ntp_synced
 notification_sleep = 5
 loop_count = 0
 while True:
@@ -247,19 +311,21 @@ while True:
         connect_wireless(wlan)
         if not wlan.isconnected():
             print("Could not reconnect to network")
-    readings = take_readings(SAMPLE_WINDOW,
-                             have_external_sensors=HAVE_EXTERNAL_SENSORS)
+    readings = take_readings(SAMPLE_WINDOW, have_external_sensors=HAVE_EXTERNAL_SENSORS)
     if HAVE_SD:
-        write_readings_to_logfile(logfile, readings, STATION_MAC)
+        write_readings_to_logfile(logfile, readings, SENSOR_STATION_ID)
     success = False
     if wlan.isconnected():
-        iline = readings_to_influxdb_line(readings,
-                                          STATION_MAC,
-                                          include_timestamp=include_timestamp)
-        success = send_data(iline)
+        success = send_data_to_influx(
+            influx_schema,
+            MEASUREMENT,
+            sensor_influx_tags,
+            readings,
+            local_timestamp=LOCAL_TIMESTAMP,
+        )
     pycom.heartbeat(False)
     if success:
-        pycom.rgbled(LED['green'])
+        pycom.rgbled(LED["green"])
     else:
-        pycom.rgbled(LED['orange'])
+        pycom.rgbled(LED["orange"])
     utime.sleep(notification_sleep)
