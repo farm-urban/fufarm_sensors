7b54d8191eb2540637577eada64213e5345f95e4
jmht <linucks42@gmail.com>
2022-01-17 18:12:45 +0000

    Updated sensor corrections

--- sensors/wipy/pysense/main.py
+++ sensors/wipy/pysense/main.py
@@ -26,22 +26,22 @@ SENSOR_CORRECTIONS = {
     "farmwipy1": {
         "barometer_temperature": -6.5,
         "humidity_temperature": -8.35,
-        "humidity_humidity": 0.0,
+        "humidity_humidity": -20.3,
     },
     "farmwipy2": {
-        "barometer_temperature": 0.0,
-        "humidity_temperature": 0.0,
-        "humidity_humidity": 0.0,
+        "barometer_temperature": -6.3,
+        "humidity_temperature": -4.5,
+        "humidity_humidity": 7.0,
     },
     "farmwipy3": {
         "barometer_temperature": -2.3,
         "humidity_temperature": -4.15,
-        "humidity_humidity": 0.0,
+        "humidity_humidity": 1.5,
     },
     "farmwipy4": {
-        "barometer_temperature": -4.0,
-        "humidity_temperature": -5.0,
-        "humidity_humidity": 0.0,
+        "barometer_temperature": -1.0,
+        "humidity_temperature": -2.8,
+        "humidity_humidity": -34.0,
     },
     "farmwipy5": {
         "barometer_temperature": 0.0,
